e48c3cf18fd476165ce8b67f9c4e76cf31aa557e
kazukiii <kazumatsu.dev28@gmail.com>
2023-12-23 00:36:08 -0800

    update union_find

--- union_find.py
+++ union_find.py
@@ -11,8 +11,10 @@ class UnionFind:
 
     def merge(self, a, b):
         x, y = self.leader(a), self.leader(b)
-        if x == y: return
-        if abs(self.parent_size[x]) < abs(self.parent_size[y]): x, y = y, x
+        if x == y:
+            return
+        if abs(self.parent_size[x]) < abs(self.parent_size[y]):
+            x, y = y, x
         self.parent_size[x] += self.parent_size[y]
         self.parent_size[y] = x
         return
@@ -28,3 +30,12 @@ class UnionFind:
         for i in range(self.n):
             result[self.leader(i)].append(i)
         return [r for r in result if r != []]
+
+
+if __name__ == "__main__":
+    uf = UnionFind(5)  # Initialize with 5 elements
+    uf.merge(0, 2)  # Merge sets containing 0 and 2
+    uf.merge(1, 2)  # Merge sets containing 1 and 2
+    print(uf.same(0, 1))  # Check if 0 and 1 are in the same set (True)
+    print(uf.size(0))  # Get the size of the set containing 0 (3)
+    print(uf.groups())  # Get all the disjoint sets ([[0, 2, 1], [3], [4]])
