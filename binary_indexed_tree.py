baf197570c87b2c2b403fdd1abbc7f0286b26162
kazukiii <kazumatsu.dev28@gmail.com>
2024-02-03 00:00:11 -0800

    add usage for bit

--- binary_indexed_tree.py
+++ binary_indexed_tree.py
@@ -1,4 +1,4 @@
-class BIT:
+class Bit:
     def __init__(self, n):
         self.size = n
         self.tree = [0] * (n + 1)
@@ -14,3 +14,22 @@ class BIT:
         while i <= self.size:
             self.tree[i] += x
             i += i & -i
+
+
+if __name__ == "__main__":
+    # Create an instance of BIT (size is 6). Note: This implementation is 1-indexed.
+    bit = Bit(6)
+
+    # Update the sequence: Add 5 at index 2 -> [0, 5, 0, 0, 0, 0]
+    # Note: Indexing starts from 1.
+    bit.add(2, 5)
+
+    # Update the sequence: Add 3 at index 5 -> [0, 5, 0, 0, 3, 0]
+    bit.add(5, 3)
+
+    # Calculate the total from index 1 to index 4
+    # Note: Indexing starts from 1.
+    print(bit.sum(4))  # Output: 5
+
+    # Calculate the total from index 1 to index 6
+    print(bit.sum(6))  # Output: 8
